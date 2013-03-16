df5df2118e8a6437c80ad4a0516a6a2b100b0e9b
John Grabenmeier <webmaster@jooon.de>
2013-03-16 17:58:39 +0100

    Added top link for mobile version. Linked main.js, added body id=page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,42 +1,18 @@
 (function($){
-	// Smooth Scrolling
-    $('#mainNav li a').click(function(){
-      var anchor = $(this);
-      $('html, body').stop().animate({
-              scrollTop: $(anchor.attr('href')).offset().top
-          }, 'slow');
-      return false;
-    });
 	
-	function nav(wrapper,toggle,breakpoint) {
-		var windowWidth = $(window).width();
-		
-		if (windowWidth < breakpoint) {
-			
-			$('#mainNav > ul').css({
-				display: 'none'
-			});
-			
-			$(wrapper).removeAttr('style').css({
-				position: 'fixed',
-				backgroundColor: 'rgba(255,255,255,.95)',
-				top: 0,
-				margin: 0,
-				width: '100%',
-				padding: '1em'
-			});
-		} else {
-			$(wrapper).removeAttr('style');
-		}
-		
-		$('.toggleNav').click(function(){
-			$('#mainNav > ul').slideToggle();
-		});
+	// Top Link
+	if ($(window).width() < 480 ) {
+		$('.container > section').append('<a href="#page" class="toTop">top</a>');
 	}
 	
-	nav('#mainNav', '#mainNav .toggleNav', '480');
-	
-	$(window).resize(function(){
-		nav('#mainNav', '#mainNav .toggleNav', '480');
-	});
+	// Smooth Scrolling
+	$(window).load(function(){
+	    $('#mainNav li a, .toTop').click(function(){
+	      var anchor = $(this);
+	      $('html, body').stop().animate({
+	              scrollTop: $(anchor.attr('href')).offset().top
+	          }, 'slow');
+	      return false;
+	    });
+	})
 }(jQuery));
